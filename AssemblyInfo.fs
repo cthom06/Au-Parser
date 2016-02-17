795d36deb19a9aeb22c32a3ed04f7f043d7cf608
Corey Thomasson <corey@cthom06.com>
2016-02-16 22:19:02 -0500

    add fsproj

---
+++ AssemblyInfo.fs
@@ -0,0 +1,42 @@
+namespace Au.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+(* General Information about an assembly is controlled through the following 
+set of attributes. Change these attribute values to modify the information
+associated with an assembly. *)
+[<assembly: AssemblyTitle("Au")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Corey Thomasson")>]
+[<assembly: AssemblyProduct("Au")>]
+[<assembly: AssemblyCopyright("Copyright © Corey Thomasson 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+(* Setting ComVisible to false makes the types in this assembly not visible 
+to COM components.  If you need to access a type in this assembly from 
+COM, set the ComVisible attribute to true on that type. *)
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("a49d201f-a9f7-4922-962c-384f6390a65e")>]
+
+(* Version information for an assembly consists of the following four values:
+
+      Major Version
+      Minor Version 
+      Build Number
+      Revision
+
+You can specify all the values or you can default the Build and Revision Numbers 
+by using the '*' as shown below:
+[<assembly: AssemblyVersion("1.0.*")>] *)
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+do
+    ()
+
