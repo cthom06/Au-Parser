dcfa8094b92e1b029d653d57243cf7fcbd2c8587
Corey Thomasson <cthomasson@vantageconsultinggroup.com>
2016-02-17 20:57:53 -0500

    fix fsproj to not include core.dll; add Parser.Pick

--- src/Parser.fs
+++ src/Parser.fs
@@ -1,37 +1,26 @@
 ﻿namespace Cthom06.Au
 
-type Parser<'a> =
+type Parser<'char, 'a> =
     | Done of 'a
     | End of 'a
-    | Except of char list * (char -> 'a Parser)
-    | Range of char list * (char -> 'a Parser)
-    | Or of 'a Parser Lazy * 'a Parser Lazy
-    with
-        override this.ToString () =
-            match this with
-            | Done _ -> "[Completed Computation]"
-            | End _ -> "[End of Input]"
-            | Except (cs, _) -> "[Any except " + System.String.Join(" ", (cs |> Seq.map string)) + "]"
-            | Range (cs, _) -> "[Any of " + System.String.Join(" ", (cs |> Seq.map string)) + "]"
-            | Or (p1, p2) -> p1.ToString () + " or " + p2.ToString ()
+    | Pick of ('char -> Parser<'char, 'a> option)
+    | Or of Parser<'char, 'a> Lazy * Parser<'char, 'a> Lazy
 
 module Parser =
-    type Error<'a> = | Success of 'a | Error of ('a Parser * char list)
+    type Error<'char,'a> = | Success of 'a | Error of (Parser<'char,'a> * 'char list)
 
     let rec bind p f =
         match p with
         | Done a
         | End a -> f a
-        | Except (cs, g) -> Except (cs, fun c -> bind (g c) f)
-        | Range (cs, g) -> Range (cs, fun c -> bind (g c) f)
+        | Pick g -> Pick (g >> Option.map (fun ng -> bind ng f))
         | Or (g, h) -> Or (lazy (bind g.Value f), lazy (bind h.Value f))
-        
+
     let rec map p f =
         match p with
         | Done a -> Done (f a)
         | End a -> End (f a)
-        | Except (cs, g) -> Except (cs, fun c -> map (g c) f)
-        | Range (cs, g) -> Range (cs, fun c -> map (g c) f)
+        | Pick g -> Pick (g >> Option.map (fun ng -> map ng f))
         | Or (g, h) -> Or (lazy (map g.Value f), lazy (map h.Value f))
 
     let rec orOf steps =
@@ -44,10 +33,10 @@ module Parser =
         match p, l with
         | Done v, xs -> Success v, xs
         | End v, [] -> Success v, []
-        | Range (cs, f), x::xs when List.contains x cs ->
-            eval (f x) xs
-        | Except (cs, f), x::xs when not (List.contains x cs) ->
-            eval (f x) xs
+        | Pick cf, x::xs ->
+            match cf x with
+            | Some v -> eval v xs
+            | None -> Error (p, l), l
         | Or (p1, p2), l ->
             match eval p1.Value l with
             | Error (e1,r1), _ ->
@@ -61,10 +50,12 @@ module Parser =
             | Success v, xs -> Success v, xs
         | p, xs -> Error (p, xs), l
         
-    let char c v = Range ([c], (fun _ -> v))
+    let filter cf f = Pick (fun c -> if cf c then Some (f c) else None)
 
-    let word s v =
-        bind (Seq.fold (fun d c -> bind d  (fun _ -> char c (Done ()))) (Done ()) s) (fun _ -> Done v)
+    let exactly c v = filter ((=) c) (fun _ -> v)
+
+    let sequence s v =
+        bind (Seq.fold (fun d c -> bind d  (fun _ -> exactly c (Done ()))) (Done ()) s) (fun _ -> Done v)
         
     let repeat p =
         let rec inner d =
@@ -73,14 +64,17 @@ module Parser =
         inner []
         
     let rec eat a b =
-        Or ( lazy (Range (a, fun _ -> eat a b)),
+        Or ( lazy (filter a (fun _ -> eat a b)),
              lazy (b) )
         
-    let eatWs b = eat [' '; '\r'; '\n'; '\t'] b
+    let eatWs b = eat System.Char.IsWhiteSpace b
+
+    let range r f = filter (fun c -> List.exists ((=) c) r) f
+    let except r f = filter (fun c -> not <| List.exists ((=) c) r) f
         
-    module Operators =
-        let (>>=) a b = bind a b
-        let ( *> ) a b = a >>= fun _ -> b
-        let (<*>) a b = map a b // <$>
-        let (<|>) a b = Or (a, b)
-        let (<~>) a b = Or (lazy (a), lazy (b))
+module Operators =
+    let (>>=) a b = Parser.bind a b
+    let ( *> ) a b = a >>= fun _ -> b
+    let (<*>) a b = Parser.map a b // <$>
+    let (<|>) a b = Or (a, b)
+    let (<~>) a b = Or (lazy (a), lazy (b))
