2ce3519d70150f53cf1bcfe9acfc3fef5fef85d7
Corey Thomasson <corey@cthom06.com>
2016-02-19 11:15:17 -0500

    fix violation of bind by removing End and allow Pick to signal succes / failure at end of stream

--- src/Parser.fs
+++ src/Parser.fs
@@ -2,8 +2,7 @@
 
 type Parser<'char, 'a> =
     | Done of 'a
-    | End of 'a
-    | Pick of ('char -> Parser<'char, 'a> option)
+    | Pick of ('char option -> Parser<'char, 'a> option)
     | Or of Parser<'char, 'a> * Parser<'char, 'a> Lazy
 
 module Parser =
@@ -11,67 +10,69 @@ module Parser =
 
     let rec bind p f =
         match p with
-        | Done a
-        | End a -> f a
+        | Done a -> f a
         | Pick g -> Pick (g >> Option.map (fun ng -> bind ng f))
-        | Or (g, h) -> Or (bind g f, lazy (bind h.Value f))
+        | Or (g, h) -> Or (bind g f, lazy(bind h.Value f))
 
     let rec map p f =
         match p with
         | Done a -> Done (f a)
-        | End a -> End (f a)
         | Pick g -> Pick (g >> Option.map (fun ng -> map ng f))
         | Or (g, h) -> Or (map g f, lazy (map h.Value f))
 
     let rec orOf (steps : _ Lazy list) =
         match steps with
         | a::b::[] -> Or (a.Value, b)
-        | x::xs -> Or (x.Value, lazy(orOf xs))
+        | x::xs -> Or (x.Value, lazy (orOf xs))
         | [] -> raise (new System.InvalidOperationException ())
 
     let eval p l =
         let rec evalStack p l stack =
-            let inline fail (stack : _ Lazy list) p xs l =
+            let inline fail (stack : _  Lazy list) p xs l =
                 match stack with
                     | [] -> Error (p, xs), l
                     | cont::nfails -> evalStack cont.Value l nfails
             match p, l with
             | Done v, xs -> Success v, xs
-            | End v, [] -> Success v, []
+            | Pick cf, [] ->
+                match cf None with
+                | Some v -> evalStack v [] stack
+                | None -> fail stack p l l
             | Pick cf, x::xs ->
-                match cf x with
+                match cf (Some x) with
                 | Some v -> evalStack v xs stack
                 | None -> fail stack p l l
             | Or (p1, p2), l ->
                 evalStack p1 l (p2::stack)
-            | p, xs -> fail stack p xs l
         evalStack p l []
         
-    let filter cf f = Pick (fun c -> if cf c then Some (f c) else None)
+    let filter cf f = Pick (Option.bind (fun c -> if cf c then Some (f c) else None))
 
     let exactly c v = filter ((=) c) (fun _ -> v)
 
     let sequence s v =
         bind (Seq.fold (fun d c -> bind d  (fun _ -> exactly c (Done ()))) (Done ()) s) (fun _ -> Done v)
-        
+
     let repeat p =
         let rec inner d =
             Or ( bind p (fun v -> inner (v::d)),
-                 lazy(Done (List.rev d)))
+                     lazy (Done (List.rev d)))
         inner []
         
-    let rec eat a b =
-        Or ( filter a (fun _ -> eat a b),
-             Lazy.CreateFromValue b )
+    let rec eat p =
+        Or ( bind p (fun _ -> p),
+             Lazy.CreateFromValue (Done ()) )
         
-    let eatWs b = eat System.Char.IsWhiteSpace b
+    let eatWs = eat (filter System.Char.IsWhiteSpace (fun _ -> Done ()))
 
     let range r f = filter (fun c -> List.exists ((=) c) r) f
     let except r f = filter (fun c -> not <| List.exists ((=) c) r) f
+
+    let endData v = Pick (fun c -> match c with | None -> Some (Done v) | _ -> None)
         
 module Operators =
     let (>>=) a b = Parser.bind a b
     let ( *> ) a b = a >>= fun _ -> b
     let (<*>) a b = Parser.map a b // <$>
-    let (<|>) a b = Or (a, b)
-    let (<~>) a b = Or (a, Lazy.CreateFromValue b)
+    let (<~>) a b = Or (a, b)
+    let (<|>) a b = Or (a, Lazy.CreateFromValue b)
